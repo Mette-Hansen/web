1fa28b155d1c7010f12245e59b77dd49d2a1bd20
Mette-Hansen <meha419@student.sdu.dk>
2021-09-21 18:25:10 +0200

    No more

--- code.js
+++ code.js
@@ -1,18 +1,28 @@
+let parent = document.getElementById("track_parent");
+let trackInsert = '<input type="text" name="track" class="inputForm" id="remove"/>';
+let additionalTracks = 1;
+
+
 function addElement() {
-    alert("Works, add");
-    let label = document.createElement("label");
-    let tnode = document.createTextNode("text");
-    label.appendChild(tnode);
-    document.body.appendChild(label);
+    //alert("Works, add");
+    // let label = document.createElement("label");
+    // let tnode = document.createTextNode("text");
+    // label.appendChild(tnode);
+    // document.body.appendChild(label);
+    if (additionalTracks < 20) {
+       parent.insertAdjacentHTML("beforeend",trackInsert);
+       additionalTracks++;
+    }
+    
 }
 function removeElement() {
-    alert("Works. remove");
+    //alert("Works. remove");
     let elem = document.getElementById("remove");
-    document.body.removeChild(elem);
+    parent.removeChild(elem);
 }
 
 function alterElement() {
-    alert("Works, alter");
+    //alert("Works, alter");
     para = document.getElementsByClassName("grid track");
     para.innerHTML = "HI!";
 }
