26df23b2c818b373c71d913d1378b8ef46fd55ad
Mette Hansen <meha419@student.sdu.dk>
2021-10-02 23:47:18 +0200

    lec03 added

--- solution/solutions/lec02sol/scripts/ex03.js
+++ solution/solutions/lec02sol/scripts/ex03.js
@@ -4,31 +4,30 @@ $(document).ready(function () {
 
 function validateSubmit() {
   $("form").on('submit', function (e) {
-    let formValid = {valid:true};
+    let formValid = { valid: true };
     let input = $('input[name=name]');
-    if(!input.val())
+    //validateTracks(formValid); 
+
+    if (!input.val()) //If there is no value in album name -> not valid
       alertNonValid(input, formValid);
 
-    input = $('input[name=year]');
-    if(!(input.val() && Number(input.val()) < 2023))
-      alertNonValid(input,formValid);
+    input = $('input[name=year]');  //Overwrite to year
+    if (!(input.val() && Number(input.val()) < 2023)) //If there is no value and if the number is over 2023 -> not valid
+      alertNonValid(input, formValid);
 
-    if(!$('input[name=type]:checked').length)
-      alertNonValid($('input[name=type]').first(), formValid);
+    if (!$('input[name=type]:checked').length) //If the inputgroup is not checked -> not valid
+      alertNonValid($('input[name=type]').first(), formValid); //It will alert nonValid if nothing (the first value) is not checked off
 
-    validateTracks(formValid);
-
-    if(!formValid.valid)
+    if (!formValid.valid) //If false, it will alert
       alert('The form has unvalid values');
-
-    return formValid.valid;
-  }); 
+      return formValid.valid;
+  });
 }
 
 function validateTracks(formValid) {
-  $('input[name=track]').each(function(index, value) {
+  $('input[name=track]').each(function (index, value) {
     let input = $(this);
-    if(!input.val())
+    if (!input.val())
       alertNonValid(input, formValid);
   });
 }
